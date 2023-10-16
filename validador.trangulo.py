52c93cea14b90ed6a4b523a941174c90e7f37561
Gui1304 <130668551+Gui1304@users.noreply.github.com>
2023-10-16 15:51:06 -0300

    commit de validação de triagulo

---
+++ validador.trangulo.py
@@ -0,0 +1,9 @@
+# validador de triangulo
+n1 = float(input('\033[0;49;97mdigite um comprimento\n'))
+n2 = float(input('digite um outro comprimento\n'))
+n3 = float(input('digite um outro comprimento\n'))
+
+if n1 < n2+n3 and n2 < n1+n3 and n3 < n1+n2:
+    print('\033[0;49;92mForma um Triangulo seu Rapaz')
+else:
+    print('\033[0;49;31mNão forma um Triangulo seu Rapaz')
