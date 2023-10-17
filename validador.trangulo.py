d3624c5751c4d6017e216f8d1163127ab576b97d
vinicius vieira <viniciusvieiravenancio@gmail.com>
2023-10-17 13:43:25 -0300

    commit

--- validador.trangulo.py
+++ validador.trangulo.py
@@ -1,9 +1,24 @@
-# validador de triangulo
-n1 = float(input('\033[0;49;97mdigite um comprimento\n'))
+# Validador de Triângulo
+
+n1 = float(input('digite um comprimento\n'))
 n2 = float(input('digite um outro comprimento\n'))
 n3 = float(input('digite um outro comprimento\n'))
 
 if n1 < n2+n3 and n2 < n1+n3 and n3 < n1+n2:
-    print('\033[0;49;92mForma um Triangulo seu Rapaz')
+    print(f'{n1},{n2} e {n3} formam um triangulo')
 else:
-    print('\033[0;49;31mNão forma um Triangulo seu Rapaz')
+    print(f'{n1},{n2} e {n3} não formam um triangulo')
+
+def tipo_trian (n1,n2,n3):
+    if n1 + n2 > n3 and n1 + n3 > n2 and n2 + n3 > n1:
+        if n1 == n2 == n3:
+            return 'triangulo equilatero'
+        elif n1 == n2 or n1 == n3 or n2 == n3:
+            return 'triangulo isosceles'
+        else:
+            return 'triangulo escaleno'
+    else:
+        return 'não é um triangulo valido'
+
+resultado = tipo_trian (n1,n2,n3)
+print(resultado)
